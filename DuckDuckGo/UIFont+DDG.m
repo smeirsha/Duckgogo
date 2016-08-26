b81cb56099d3237c39486b56729e980e6a2b4c9d
Simina Pasat <siminap@microsoft.com>
2016-08-26 14:52:54 -0700

    Changing licensed font with default one

--- DuckDuckGo/UIFont+DDG.m
+++ DuckDuckGo/UIFont+DDG.m
@@ -10,18 +10,18 @@
 
 @implementation UIFont (DDG)
 
-+(UIFont*)duckStoryTitle { return [UIFont fontWithName:@"ProximaNova-Regular" size:16.0f]; }
++(UIFont*)duckStoryTitle { return [UIFont fontWithName:@"Courier" size:16.0f]; }
 
-+(UIFont*)duckStoryTitleLarge { return [UIFont fontWithName:@"ProximaNova-Regular" size:24.0f]; }
++(UIFont*)duckStoryTitleLarge { return [UIFont fontWithName:@"Courier" size:24.0f]; }
 
-+(UIFont*)duckStoryTitleSmall  { return [UIFont fontWithName:@"ProximaNova-Regular" size:14.0f]; }
++(UIFont*)duckStoryTitleSmall  { return [UIFont fontWithName:@"Courier" size:14.0f]; }
 
-+(UIFont*)duckStoryCategory { return [UIFont fontWithName:@"ProximaNova-Regular" size:12.0f]; }
++(UIFont*)duckStoryCategory { return [UIFont fontWithName:@"Courier" size:12.0f]; }
 
-+(UIFont*)duckStoryCategorySmall { return [UIFont fontWithName:@"ProximaNova-Regular" size:12.0f]; }
++(UIFont*)duckStoryCategorySmall { return [UIFont fontWithName:@"Courier" size:12.0f]; }
 
-+(UIFont*)duckGeneral { return [UIFont fontWithName:@"ProximaNova-Regular" size:12.0f]; }
++(UIFont*)duckGeneral { return [UIFont fontWithName:@"Courier" size:12.0f]; }
 
-+(UIFont*)duckFontWithSize:(CGFloat)size { return [UIFont fontWithName:@"ProximaNova-Regular" size:size]; }
++(UIFont*)duckFontWithSize:(CGFloat)size { return [UIFont fontWithName:@"Courier" size:size]; }
 
 @end
